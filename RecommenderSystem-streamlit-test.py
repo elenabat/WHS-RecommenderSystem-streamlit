22d902beb087f1b07cb1014f86b5148052e288ad
elenabat <109755093+elenabat@users.noreply.github.com>
2022-08-04 09:47:44 +0200

    Update RecommenderSystem-streamlit-test.py

--- RecommenderSystem-streamlit-test.py
+++ RecommenderSystem-streamlit-test.py
@@ -211,7 +211,7 @@ data_country2 = data_country2.sort_values(by=['count'], ascending = False).reset
 data_country2 = data_country2.drop('index' , axis = 1)
 
 #Treemap
-fig = px.treemap(data_country2, path=[px.Constant("world"), 'region_en', 'states_name_en', 'name_en'], values='count',
+fig = px.treemap(data_country2, path=['region_en', 'states_name_en', 'name_en'], values='count',
                 color='region_en', hover_data=['iso_code'], color_discrete_map={"world": "orange", "Europe": "#4F84C4", "North America": "#926AA6", "Latin America and the Caribbean": "#CE3175", "Africa": "#92B558", "Middle East": "#D8AE47", "Asia and the Pacific": "#47b8b8"})
 st.plotly_chart(fig)
  
